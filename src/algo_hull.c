f884bc6d948f43812c4f08373a79108af9212945
madx <root@yapok.org>
2010-03-09 23:21:35 +0100

    placeholder for graham scan

--- src/algo_hull.c
+++ src/algo_hull.c
@@ -2,17 +2,10 @@
 #include <stdlib.h>
 #include "util.h"
 #include "polygon.h"
+#include "stack.h"
 #include "algorithms.h"
 #include "ui.h"
 
 void gui_graham_convex_hull_algorithm (gui *g) {
-  direction dir;
-
-  if (g->poly->size == 3) {
-    gui_algorithm_info (g, "Graham", "this is a triangle");
-    return;
-  }
-
-  dir = poly_get_direction (g->poly);
-
+  puts ("Unimplemented");
 }
