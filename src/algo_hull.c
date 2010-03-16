8c2cccb78d2d829ff9c968077fc7aa5f78230b28
madx <root@yapok.org>
2010-03-16 15:58:54 +0100

    working graham scan

--- src/algo_hull.c
+++ src/algo_hull.c
@@ -6,25 +6,11 @@
 #include "algorithms.h"
 #include "ui.h"
 
-/* Trouver le pivot P;
-   Trier les points par angle (les points d'angle égal seront triés par rapport à leur abscisse);
-
-   # Points[0] est le pivot, Points[longueur] aussi (fin du circuit)
-   Pile.empiler(Points[0]);
-   Pile.empiler(Points[1]);
-   POUR i = 2 A Points.longueur
-           TANT QUE (Pile.hauteur >= 2) ET (Produit_vectoriel(Pile.second, Pile.haut, Points[i]) <= 0)
-                   Pile.dépiler;
-           FIN TANT QUE
-           Pile.empiler(Points[i]);
-   FIN POUR
-*/
-
 extern struct colors_t Color;
 
 vertex *pivot;
 
-void graham_show_names (gui *g);
+/* void graham_show_names (gui *g, vertex **points, int n); */
 
 void gui_graham_convex_hull_algorithm (gui *g) {
   vertex **points;
@@ -33,9 +19,9 @@ void gui_graham_convex_hull_algorithm (gui *g) {
   int      i, pindex;
   direction dir = poly_get_direction (g->poly);
 
-  graham_show_names (g);
-
   points = poly_to_array (g->poly);
+  /* graham_show_names (g, points, g->poly->size); */
+
   points = realloc (points, (g->poly->size+1) * sizeof(*points));
   assert (NULL != points);
 
@@ -49,43 +35,35 @@ void gui_graham_convex_hull_algorithm (gui *g) {
       pivot = points[pindex];
     }
   }
-  t = points[0];
-  points[0] = pivot;
-  points[pindex] = t;
+  array_swap (points, 0, pindex);
   points[g->poly->size] = pivot;
-
-  puts("");
-  for (i = 0; i < g->poly->size; i++)
-    vertex_print (points[i]);
 
   /* Tri des points par angle avec le pivot */
   qsort (points+1, g->poly->size - 1, sizeof(*points), graham_sort_points);
-
-  puts("");
-  for (i = 0; i < g->poly->size; i++)
-    vertex_print (points[i]);
 
   /* Déroulement de l'algo */
   s = stack_new (g->poly->size + 1);
   stack_push (s, points[0]);
   stack_push (s, points[1]);
 
-  for (i = 0; i < g->poly->size + 1; i++) {
-    while ((dir == CCW) ? (s->cur >= 2) &&
+  for (i = 2; i < g->poly->size + 1; i++) {
+    while ((s->cur >= 2) &&
            vertex_cross ((vertex*) stack_peekn (s, 2),
                          (vertex*) stack_peek  (s),
                          points[i]) > 0
-                        : (s->cur >= 2) &&
-           vertex_cross ((vertex*) stack_peekn (s, 2),
-                         (vertex*) stack_peek  (s),
-                         points[i]) < 0
-           ) {
+          ) {
       stack_pop (s);
     }
     stack_push (s, points[i]);
   }
 
-  stack_print (s);
+  /* Dessin */
+  while (s->cur > 1) {
+    t = stack_pop (s);
+    gui_draw_edge (g, t, ((vertex*) stack_peek (s)), Color.outline);
+    gui_draw_vertex (g, t, Color.low, Color.outline);
+  }
+  gui_draw_vertex (g, ((vertex*) stack_peek (s)), Color.low, Color.outline);
 
   stack_free (s);
   free (points);
@@ -101,24 +79,27 @@ int graham_sort_points (const void *p1, const void *p2) {
 
   diff = a1 - a2;
 
-  return (diff < 0) ? -1 : (diff == 0) ? 0 : 1;
+  if (diff < 0) {
+    return -1;
+  } else if (diff == 0) {
+
+  }
+
+  return 1;
 }
 
-void graham_show_names (gui *g) {
+void graham_show_names (gui *g, vertex **points, int n) {
   PangoLayout *layout;
   char buf[10];
-  vertex *t;
+  int i;
 
   gc_set_fg (g->gc, "black");
 
-  for (t = g->poly->hull; t != g->poly->last; t = t->next) {
-    g_sprintf (buf, "%p", (void*) t);
+  for (i = 0; i < n; i++) {
+    g_sprintf (buf, "%p", (void*) points[i]);
     layout = gtk_widget_create_pango_layout (g->draw_zone, buf);
-    gdk_draw_layout (g->pixmap, g->gc, t->x - 8, t->y - 16, layout);
+    gdk_draw_layout (g->pixmap, g->gc, points[i]->x - 8, points[i]->y - 16, layout);
   }
-  g_sprintf (buf, "%p", (void*) t);
-  layout = gtk_widget_create_pango_layout (g->draw_zone, buf);
-  gdk_draw_layout (g->pixmap, g->gc, t->x - 8, t->y - 16, layout);
 
   gtk_widget_queue_draw (g->draw_zone);
 }
--- src/util.h
+++ src/util.h
@@ -8,4 +8,6 @@ typedef enum { false, true } bool;
 #define Alloc(p) {p = malloc (sizeof(*p)); assert (NULL != p);}
 #define AllocN(p, n) {p = malloc (n * sizeof(*p)); assert (NULL != p);}
 
+#define array_swap(a, i, j) do {void* p = a[i]; a[i] = a[j]; a[j] = p;} while (0)
+
 #endif
